9cc562a4d4cac7d8581ca4a0f650009e62d6616e
Souvik Dey Sarkar <sdeys2111@gmail.com>
2022-04-28 22:33:57 +0530

    Add operator event listener

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
 // variable declarations
 let firstNum = '';
 let secondNum = '';
+let opetator = '';
 const display = document.querySelector(".display");
 const clearBtn = document.querySelector(".clear");
 const numbers = Array.from(document.querySelectorAll(".number"));
+const addBtn = document.querySelector(".add");
+const subtractBtn = document.querySelector(".subtract");
+const multiplyBtn = document.querySelector(".multiply");
+const divideBtn = document.querySelector(".divide");
 
 // event listener declarations
-numbers.forEach((number) => {
-    number.addEventListener("click", getNumber);
-});
-
+numbers.forEach(number => number.addEventListener("click", getNumber));
 clearBtn.addEventListener("click", clearScreen);
+addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
+
 
 // function declarations
 function getNumber(e) {
@@ -22,4 +29,5 @@ function clearScreen() {
     display.textContent = '';
     firstNum = '';
     secondNum = '';
+    opetator = '';
 }
