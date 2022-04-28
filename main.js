be24389afc9ca9089db7d5af220ef552c0238ffd
Souvik Dey Sarkar <sdeys2111@gmail.com>
2022-04-28 23:12:07 +0530

    Add getOperator and change operator event listener

--- main.js
+++ main.js
@@ -1,33 +1,47 @@
 // variable declarations
 let firstNum = '';
 let secondNum = '';
-let opetator = '';
+let operator = '';
 const display = document.querySelector(".display");
 const clearBtn = document.querySelector(".clear");
 const numbers = Array.from(document.querySelectorAll(".number"));
-const addBtn = document.querySelector(".add");
-const subtractBtn = document.querySelector(".subtract");
-const multiplyBtn = document.querySelector(".multiply");
-const divideBtn = document.querySelector(".divide");
+// const addBtn = document.querySelector(".add");
+// const subtractBtn = document.querySelector(".subtract");
+// const multiplyBtn = document.querySelector(".multiply");
+// const divideBtn = document.querySelector(".divide");
+const operators = Array.from(document.querySelectorAll(".operator"));
 
 // event listener declarations
 numbers.forEach(number => number.addEventListener("click", getNumber));
 clearBtn.addEventListener("click", clearScreen);
-addBtn.addEventListener("click", add);
-subtractBtn.addEventListener("click", subtract);
-multiplyBtn.addEventListener("click", multiply);
-divideBtn.addEventListener("click", divide);
+operators.forEach(operator => operator.addEventListener("click", getOperator));
+// addBtn.addEventListener("click", add);
+// subtractBtn.addEventListener("click", subtract);
+// multiplyBtn.addEventListener("click", multiply);
+// divideBtn.addEventListener("click", divide);
 
 
 // function declarations
 function getNumber(e) {
-    firstNum += e.target.textContent;
-    display.textContent = firstNum;
+    if (operator.length === 0) {
+        firstNum += e.target.textContent;
+        display.textContent = firstNum;
+    } else {
+        display.textContent = '';
+        secondNum += e.target.textContent;
+        display.textContent = secondNum;
+    }
+    console.log(firstNum);
+    console.log(secondNum);
 }
 
 function clearScreen() {
     display.textContent = '';
     firstNum = '';
     secondNum = '';
-    opetator = '';
+    operator = '';
+}
+
+function getOperator(e) {
+    operator = e.target.textContent;
 }
