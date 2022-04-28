20c2aea96e1a1ba77bc4b414d96c6c98a005f383
Souvik Dey Sarkar <sdeys2111@gmail.com>
2022-04-29 00:42:58 +0530

    Add operate function to calculate

--- main.js
+++ main.js
@@ -5,21 +5,14 @@ let operator = '';
 const display = document.querySelector(".display");
 const clearBtn = document.querySelector(".clear");
 const numbers = Array.from(document.querySelectorAll(".number"));
-// const addBtn = document.querySelector(".add");
-// const subtractBtn = document.querySelector(".subtract");
-// const multiplyBtn = document.querySelector(".multiply");
-// const divideBtn = document.querySelector(".divide");
 const operators = Array.from(document.querySelectorAll(".operator"));
+const equalBtn = document.querySelector(".equal");
 
 // event listener declarations
 numbers.forEach(number => number.addEventListener("click", getNumber));
 clearBtn.addEventListener("click", clearScreen);
 operators.forEach(operator => operator.addEventListener("click", getOperator));
-// addBtn.addEventListener("click", add);
-// subtractBtn.addEventListener("click", subtract);
-// multiplyBtn.addEventListener("click", multiply);
-// divideBtn.addEventListener("click", divide);
-
+equalBtn.addEventListener("click", operate);
 
 // function declarations
 function getNumber(e) {
@@ -44,4 +37,36 @@ function clearScreen() {
 
 function getOperator(e) {
     operator = e.target.textContent;
+    console.log(operator);
+}
+
+function operate() {
+    if (firstNum.length === 0 || secondNum.length === 0 || operator.length === 0) {
+        return;
+    }
+    if (operator === "+") {
+        add();
+    } else if (operator === "-") {
+        subtract();
+    } else if (operator === "*"){
+        multiply();
+    } else if (operator === "/") {
+        divide();
+    }
+}
+
+function add() {
+    display.textContent = `${parseInt(firstNum) + parseInt(secondNum)}`;
+}
+
+function subtract() {
+    display.textContent = `${parseInt(firstNum) - parseInt(secondNum)}`;
+}
+
+function multiply() {
+    display.textContent = `${parseInt(firstNum) * parseInt(secondNum)}`;
+}
+
+function divide() {
+    display.textContent = `${parseInt(firstNum) / parseInt(secondNum)}`;
 }
